927d8ff19e4ea8629e95aee127a3587f3d1c828d
Isaac Romarri <iromero385@berkeley.edu>
2018-08-31 01:12:53 -0700

    add functuanally for multiple games and guess

--- index.js
+++ index.js
@@ -3,43 +3,55 @@ var WordBank = require("./wordBank.js")
 var inquirer = require("inquirer")
 var newGuess = new Word(WordBank[6]);
 var userName; 
-var gameOver = false
-// inquirer
-//   .prompt([
-//     // Here we create a basic text prompt.
-//     {
-//       type: "input",
-//       message: "What is your name?",
-//       name: "username"
-//     }
-//   ])
-//   .then(function(answers) {
-//     userName = answers.username;
-//     runGame(); 
-//   });
-runGame();
+var numberofGuesses = 0; 
+inquirer
+  .prompt([
+    // Here we create a basic text prompt.
+    {
+      type: "input",
+      message: "Welcome to the 90's movies Hangman \n What is your name?",
+      name: "username"
+    }
+  ])
+  .then(function(answers) {
+    userName = answers.username;
+    runGame(); 
+  });
+
 function runGame() {
     console.log(newGuess.wordDisplay())
+    console.log("Number of Guess left " + (10-numberofGuesses))
     if(!newGuess.correctGuess){
       inquirer
         .prompt([
           {
             type: "input",
             message: "Pick a letter",
-            name: "letter",
+            name: "letters",
             validate: function (value) {
               var letters = /^[A-Za-z]+$/;
-              if (value.match(letters) && value.length==1) {
+              var counter = 0;
+              for (var i = 0; i < value.length; i++){
+                if (value[i].match(letters) ) {
+                  counter++;
+                }
+              }
+              numberofGuesses=+counter;
+              if(counter == value.length){
                 return true;
               }
-              else {
+              else{
                 return false;
               }
+  
+              
             }
           }
         ])
         .then(function(response) {
-          newGuess.checkAllLetters(response.letter.toLowerCase())
+          for(var i=0; i < response.letters.length; i++){
+            newGuess.checkAllLetters(response.letters[i])
+          }
           newGuess.wordDisplay();
           newGuess.wordIsGuess();
           runGame();
@@ -47,17 +59,33 @@ function runGame() {
 
     }
     else{
-      console.log("You are a Winner")
-      console.log("Try again?")
-      resetGame();
-      runGame();
+      console.log( userName+ " Won!")
+      newGame();
     }
-      
-  
-
 }
 function resetGame(){
-  var seed = Math.floor(Math.random()*WordBank.length)
+  var seed = Math.floor(Math.random()*WordBank.length);
   newGuess = new Word(WordBank[seed]);
+  numberofGuesses = 0;
   
+}
+function newGame(){
+  
+  inquirer
+        .prompt([
+          {
+            type: "confirm",
+            message: "Would you like to play again " + userName,
+            name: "newGame"
+          }
+        ])
+        .then(function(response) {
+          if(response.newGame){
+            resetGame();
+            runGame();
+          }
+          else{
+            console.log("Thank you for Playing \n"+"Bye "+ userName);
+          }
+        });
 }
